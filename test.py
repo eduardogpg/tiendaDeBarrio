08b49dadb29accea33d047a06285d533d07a7c6d
Eduardo Ismael <eduardo78d@gmail.com>
2023-09-29 18:09:38 -0600

    Todo bien

--- test.py
+++ test.py
@@ -2,8 +2,12 @@ import unittest
 
 class TestExample(unittest.TestCase):
 
-    def test_example(self):
-        self.assertEqual(8, 8) 
+    def test_example1(self):
+        self.assertEqual(1, 1)
+
+
+    def test_example2(self):
+        self.assertEqual(0, 0)
 
 
 if __name__ == '__main__':
